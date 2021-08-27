5a29ace3174cc8217eb117e05ae4cd0f7a410c3f
= <tannorfranks@gmail.com>
2021-08-27 18:37:45 -0500

    Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import './App.scss';
+import {Dashboard} from "./app/Components/Dashboard/Dashboard";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+            <div>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/foo">Foo</Link>
+                    <Link to="/bar">Bar</Link>
+                </nav>
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+               {/*<Route exact path="/foo" component={Foo} />*/}
+               {/*     <Route exact path="/bar" component={Bar} />*/}
+                </Switch>
+            </div>
+        </Router>
   );
 }
 
