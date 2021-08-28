c3b4251f41e36994cd205b21413fe484558bbb67
= <tannorfranks@gmail.com>
2021-08-27 22:54:40 -0500

    Add mo stuff

--- src/app/Components/LeftNav/LeftNav.tsx
+++ src/app/Components/LeftNav/LeftNav.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import {NavItem} from "./NavItem";
+import React from "react";
+import { NavItem } from "./NavItem";
 
 export const LeftNav = () => {
-    const routes = [
-        {
-            header: 'Dashboard',
-            path: ''
-        },
-        {
-            header: 'Cool Shit',
-            path: '/cool-shit'
-        },
-    ]
+  const routes = [
+    {
+      header: "Dashboard",
+      path: "",
+    },
+    {
+      header: "Cool Shit",
+      path: "cool-shit",
+    },
+    {
+      header: "Jakes house",
+      path: "shit-shack",
+    },
+  ];
 
-    return (
-        <nav>
-            {routes.map((route) => (
-                <NavItem header={route.header} path={route.path} key={route.header}/>
-            ))}
-        </nav>
-    )
-}
+  return (
+    <nav>
+      {routes.map((route) => (
+        <NavItem header={route.header} path={route.path} key={route.header} />
+      ))}
+    </nav>
+  );
+};
